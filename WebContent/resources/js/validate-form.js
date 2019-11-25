3e90fd3d1dac0ed21453fb4579e7308bc6ef071e
Johnny Trentin <johnny.trentin@totvs.com.br>
2019-11-25 17:39:11 -0300

    adição do .project para importação do eclipse e começo da validação do form em JS

--- WebContent/resources/js/validate-form.js
+++ WebContent/resources/js/validate-form.js
@@ -1,1 +1,18 @@
-asd
+'use strict';
+
+
+function sendForm() {
+	
+	let name = document.getElementsByName("nome")[0].value;
+	let lastName = document.getElementsByName("sobrenome")[0].value;
+	let phone = document.getElementsByName("telefone")[0].value;
+	let email = document.getElementsByName("email")[0].value;
+	let password = document.getElementsByName("password")[0].value;
+	
+	
+}
+
+function validatForm() {
+
+	
+}
